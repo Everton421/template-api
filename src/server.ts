7540d3a6f69684b65615eb45768fdb888fbaae20
evertonjose421@gmail.com <evertonjose421@gmail.com>
2024-10-10 09:25:23 -0300

    '101020240852'

--- src/server.ts
+++ src/server.ts
@@ -15,18 +15,17 @@ import { conn } from './database/databaseConfig';
 
         // Configuração do CORS
             const corsOptions = {
-                
-                //origin: '*', // Permitir todas as origens. Para maior segurança, considere especificar as origens permitidas.
+                origin: '*', // Permitir todas as origens. Para maior segurança, considere especificar as origens permitidas.
                 methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
                 allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version', 'Authorization'],
                 credentials: true, // Permitir credenciais
             };
 
 
+            app.use(cors( corsOptions));
         
         app.use(`${versao}/api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocs))
         
-        app.use(cors( corsOptions));
         app.use(express.json());    
         app.use(router)
 
