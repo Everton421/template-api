33eb7fef89177ddd58b33dcf9e1fa6df71f18339
evertonjose421@gmail.com <evertonjose421@gmail.com>
2024-11-05 15:55:49 -0300

    05112024

--- src/models/tipos_os/select.ts
+++ src/models/tipos_os/select.ts
@@ -5,7 +5,7 @@ export class SelectTipo_os{
 
     async   buscaGeral(empresa:any )   {
         return new Promise   ( async ( resolve , reject ) =>{
-        let sql = ` select * from ${empresa}.tipo_os  `
+        let sql = ` select * from ${empresa}.tipos_os  `
             await conn.query(sql,  (err, result  )=>{
                 if (err)  reject(err); 
                   resolve(result)
